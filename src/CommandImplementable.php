f40a5cba7cec32225b624870ba0c9534132b8d48
Sainan <63328889+Sainan@users.noreply.github.com>
2021-01-02 12:43:28 +0100

    Add another emoji

--- src/CommandImplementable.php
+++ src/CommandImplementable.php
@@ -4,6 +4,6 @@ abstract class CommandImplementable extends Command
 {
 	function getDefaultResponse() : string
 	{
-		return "I'm sorry, I can't do that in this form.";
+		return "I'm sorry, I can't do that in this form. :|";
 	}
 }
