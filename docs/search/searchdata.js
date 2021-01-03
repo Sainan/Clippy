56e6b5b872250c06f2a676cb6a4f1b1b8778866b
Sainan <63328889+Sainan@users.noreply.github.com>
2021-01-03 17:11:38 +0100

    Add docs

---
+++ docs/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "cdg",
+  1: "c",
+  2: "g",
+  3: "d"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions",
+  3: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Functions",
+  3: "Pages"
+};
+
