3e82b03a809145679d9fae0608143e82df53d5a1
Sainan <63328889+Sainan@users.noreply.github.com>
2021-01-23 06:52:09 +0100

    Update translations

---
+++ lang/la-LA/output.php
@@ -0,0 +1,60 @@
+<?php
+return [
+	"default" => "Ignosce mihi, non intellego. :/",
+
+	"farewell" => "Vale!",
+
+	"greeting" => "Salve!",
+
+	"implementable" => "Ignosce mihi, id non facere possum hac in implantatione. :|",
+
+	"opinion" => "Sententiam de illo non habeo. :)",
+
+	"thanks" => "Libenter! :D",
+
+	"arithmetics" => "Iste expressus aequat %s . :)",
+
+	// CommandConvert
+	"convert_singular" => "%s fere idem est quam %s. :)",
+	"convert_plural" => "%s fere idem sunt quam %s. :)",
+	// CommandConvertDistance
+	"cm_singular" => "centimetrum",
+	"cm_plural" => "centimetra",
+	"m_singular" => "metrum",
+	"m_plural" => "metra",
+	"km_singular" => "chiliometrum",
+	"km_plural" => "chiliometra",
+	"in_singular" => "uncia",
+	"in_plural" => "unciae",
+	"ft_singular" => "pes",
+	"ft_plural" => "pedes",
+	"yards_singular" => "virga",
+	"yards_plural" => "virgae",
+	"rods_singular" => "canna",
+	"rods_plural" => "cannae",
+	"chains_singular" => "catena",
+	"chains_plural" => "catenae",
+	"furlongs_singular" => "stadium",
+	"furlongs_plural" => "stadia",
+	"mi_singular" => "mille passus",
+	"mi_plural" => "mille passus",
+	// CommandConvertWeight
+	"g_singular" => "gramma",
+	"g_plural" => "grammae",
+	"kg_singular" => "chiliogramma",
+	"kg_plural" => "chiliogrammae",
+	"ounces_singular" => "uncia",
+	"ounces_plural" => "unciae",
+	"pounds_singular" => "libra",
+	"pounds_plural" => "librae",
+	"stones_singular" => "lapis",
+	"stones_plural" => "lapides",
+	"hundredweights_singular" => "centenarium",
+	"hundredweights_plural" => "centenaria",
+	"ustons_singular" => "tunna brevis",
+	"ustons_plural" => "tunnae breves",
+	"metrictons_singular" => "metreta",
+	"metrictons_plural" => "metretae",
+	"imperialtons_singular" => "tunna longa",
+	"imperialtons_plural" => "tunnae longae",
+];
---
+++ lang/sh/output.php
@@ -0,0 +1,60 @@
+<?php
+return [
+	"default" => "Izvinite, ne razumijem. :/",
+
+	"farewell" => "Doviđenja!",
+
+	"greeting" => "Zdravo!",
+
+	"implementable" => "I'm sorry, I can't do that in this embodiment. :|",
+
+	"opinion" => "I don't have an opinion on that. :)",
+
+	"thanks" => "Nema na čemu! :D",
+
+	"arithmetics" => "That expression evaluates to %s. :)",
+
+	// CommandConvert
+	"convert_singular" => "%s is about equal to %s. :)",
+	"convert_plural" => "%s are about equal to %s. :)",
+	// CommandConvertDistance
+	"cm_singular" => "centimetar",
+	"cm_plural" => "centimetara",
+	"m_singular" => "metar",
+	"m_plural" => "metara",
+	"km_singular" => "kilometar",
+	"km_plural" => "kilometara",
+	"in_singular" => "inč",
+	"in_plural" => "inča",
+	"ft_singular" => "stopa",
+	"ft_plural" => "stopa",
+	"yards_singular" => "yard",
+	"yards_plural" => "yards",
+	"rods_singular" => "rod",
+	"rods_plural" => "rods",
+	"chains_singular" => "chain",
+	"chains_plural" => "chains",
+	"furlongs_singular" => "furlong",
+	"furlongs_plural" => "furlongs",
+	"mi_singular" => "milja",
+	"mi_plural" => "milje",
+	// CommandConvertWeight
+	"g_singular" => "gram",
+	"g_plural" => "grama",
+	"kg_singular" => "kilogram",
+	"kg_plural" => "kilograma",
+	"ounces_singular" => "ounce",
+	"ounces_plural" => "ounces",
+	"pounds_singular" => "pound",
+	"pounds_plural" => "pounds",
+	"stones_singular" => "stone",
+	"stones_plural" => "stones",
+	"hundredweights_singular" => "hundredweight",
+	"hundredweights_plural" => "hundredweights",
+	"ustons_singular" => "US ton",
+	"ustons_plural" => "US tons",
+	"metrictons_singular" => "metric ton",
+	"metrictons_plural" => "metric tons",
+	"imperialtons_singular" => "imperial ton",
+	"imperialtons_plural" => "imperial tons",
+];
